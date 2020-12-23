54c07919fffd4c6881d87e9bf4a03da33b038048
imssyang <imssyang@gmail.com>
2020-12-23 15:33:44 +0800

    support ant proxy

--- setup/env.sh
+++ setup/env.sh
@@ -2,6 +2,6 @@
 
 export ANT_DISABLE_ENV=yes
 export ANT_HOME=/opt/ant
-#export ANT_OPTS="-Dhttp.proxyHost=proxy -Dhttp.proxyPort=8118"
+export ANT_OPTS="-Dhttp.proxyHost=127.0.0.1 -Dhttp.proxyPort=8118"
 export PATH=$ANT_HOME/bin:$PATH
 
